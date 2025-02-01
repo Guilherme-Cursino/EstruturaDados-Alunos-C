12fa1c1f23897354b42aee6b5710e4295efc9520
JoaoPas-Ramos <20232TADSSAJ0032@ifba.edu.br>
2025-02-01 15:22:29 -0300

    função cadastraAluno + menu

--- Projeto1CRUDAlunos.c
+++ Projeto1CRUDAlunos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 typedef struct Aluno
 {
     int id;
@@ -16,6 +17,29 @@ Aluno *cauda = NULL;
 // typedef struct Aluno Aluno;
 
 void cadastraAluno(){
+    Aluno *novo = (Aluno *)malloc(sizeof(Aluno));
+    printf("Digite o ID do aluno: ");
+    scanf("%d", &novo->id);
+    // Verificar se o id ja ta cadastrado
+    Aluno *atual = inicio;
+    while (atual != NULL) {
+        if (atual->id == novo->id) { 
+            printf("Erro: Já existe um aluno com esse ID.\n");
+            return;
+        }
+        atual = atual->proximo;
+    }
+    printf("Digite o nome do aluno: ");
+    scanf(" %[^\n]", novo->nome);
+    printf("Digite a nota do aluno: ");
+    scanf("%f", &novo->nota);
+    
+    novo->anterior = NULL;
+    novo->proximo = inicio;
+    if (inicio != NULL) inicio->anterior = novo;
+    inicio = novo;
+
+    printf("Aluno cadastrado com sucesso!\n");
 }
 
 void removeAluno(){
@@ -45,6 +69,13 @@ int main() {
     do
     {
         printf("MENU\n");
+        printf("1. Cadastrar aluno\n");
+        printf("2. Remover aluno\n");
+        printf("3. Mostrar aluno\n");
+        printf("4. Editar aluno\n");
+        printf("5. Mostrar todos os alunos\n");
+        printf("6. Mostrar estatísticas\n");
+        printf("0. Sair\n");
         printf("Selecione a operacao que deseja fazer: ");
         scanf("%d", &opcao);
 
